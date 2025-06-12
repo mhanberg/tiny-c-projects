4a95a53e7fb4b83a5973fc3e514408b40bb043c7
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-06-11 21:18:41 -0400

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tiny C Projects
+
+Implementations for the tiny projects in the book [Tiny C Projects](https://www.manning.com/books/tiny-c-projects) by Dan Gookin.
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Tiny C Projects";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    inherit (nixpkgs) lib;
+    systems = [
+      "aarch64-darwin"
+      "x86_64-darwin"
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    perSystem = func: lib.genAttrs systems (system: func {pkgs = nixpkgs.legacyPackages.${system};});
+  in {
+    devShells = perSystem ({pkgs, ...}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          clang
+          clang-analyzer
+        ];
+      };
+    });
+  };
+}
